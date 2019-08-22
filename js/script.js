57bae7c4368987cce84fe664f8e6ac6d40a100c1
Logan Stone <logan.stone20@gmail.com>
2019-08-22 03:39:48 +0000

    Done.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    document.body.innerHTML = "<p class='util--hide'>View <a href='https://learn.co/lessons/js-jquery-document-ready-readme'>Document Ready</a> on Learn.co and start learning to code for free.</p>";
+});
